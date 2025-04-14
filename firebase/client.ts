cf56a0c7f9a1ce2790cf99e6e13d5e14f1b2c28d
Ketan17-CS <ketantejam9990@gmail.com>
2025-04-14 18:49:36 +0530

    final application

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,8 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
-
 // import { getAnalytics } from "firebase/analytics";
-
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -20,8 +18,10 @@ const firebaseConfig = {
     measurementId: "G-5R98BCCGHL"
 };
 
+
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
