c03b5dee5b9276f2f7cc1a8d781fd65b95b2d30a
Ketan17-CS <ketantejam9990@gmail.com>
2025-04-14 19:22:23 +0530

    final application

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,15 +9,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBRhBmKf9_sL3jFaFX3BbsvMpnGqrBO6Rw",
-    authDomain: "prepwisw.firebaseapp.com",
-    projectId: "prepwisw",
-    storageBucket: "prepwisw.firebasestorage.app",
-    messagingSenderId: "279428001424",
-    appId: "1:279428001424:web:bab971f1691f4b90987215",
-    measurementId: "G-5R98BCCGHL"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
